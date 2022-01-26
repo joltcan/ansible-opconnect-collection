76eb0c24ee1806fcad6f7d9e1e3dbfa8680d9a6c
Fredrik Lundhag <fredrik.lundhag@impactct.com>
2022-01-26 13:00:00 +0100

    first post

---
+++ README.md
@@ -0,0 +1,91 @@
+# 1Password Connect Ansible Collection
+
+This 1Password Connect collection contains a lookup module that can easily get the password or other field value into your ansible code.
+
+You can learn more about [Secrets Automation and 1Password Connect](https://1password.com/secrets/) on 1Password website.
+
+## Table of Contents
+
+* [Requirements](#requirements)
+* [Installation](#installation)
+* [Module & Environment Variables](#module-variables)
+* [`generic_item` Module](#connectgeneric_item-module)
+* [`item_info` Module](#item_info-module)
+* [`field_info` Module](#field_info-module)
+* [Testing](#testing)
+* [About 1Password](#about-1password)
+* [Security](#security)
+
+
+### Requirements
+- Python >= 3.6.0
+- 1Password Connect >= 1.0.0
+    
+#### Supported Ansible Versions
+
+This collection has been tested against the following Ansible versions:
+* `ansible-core`: >=2.9, 2.11, 2.12
+* `ansible`: >=4.0, <5.0
+
+## Installation
+
+You can install the Ansible collection from [Ansible Galaxy](https://galaxy.ansible.com/joltcan/opconnect):
+
+```
+ansible-galaxy collection install joltcan.opconnect
+```
+
+## Plugin Variables
+
+The plugin support the following variable definitions. You may either explicitly define the value on the task or let Ansible fallback to an environment variable to use the same value across all tasks.
+
+Environment variables are ignored if the module variable is defined for a task.
+
+| Plugin Variable | Environment Variable | Description                                                                             |
+|----------------:|----------------------|-----------------------------------------------------------------------------------------|
+|        `op_connect_host` | `OP_CONNECT_HOST`           | URL of a 1Password Connect API Server                                   |
+|       `op_connect_token` | `OP_CONNECT_TOKEN`          | JWT used to authenticate 1Password Connect API requests                 |
+|   `op_connect_ca_bundle` | `OP_CONNECT_SKIP_VERIFY `   | (Optional) name of a 1Password Vault the API token is allowed to access |
+| `op_connect_skip_verify` | `OP_CONNECT_SKIP_VERIFY `   | (Optional) name of a 1Password Vault the API token is allowed to access |
+
+
+## `opconnect.generic_item` plugin
+
+
+### Example Usage
+**Get Item**
+```yaml
+---
+- name: Lookup a password
+  hosts: localhost
+  environment:
+    OP_CONNECT_HOST: http://localhost:8001
+    OP_CONNECT_TOKEN: "api.jwt.here"
+  collections:
+    - joltcan.opconnect
+  tasks:
+    - set_fact: foo_password="{{ lookup('opconnect', 'item', vault='OPS', section='creds', field='api_key') }}"
+    - debug: msg="var is {{ foo_password }} "
+    - debug: msg="{{ lookup('opconnect', 'item', vault='OPS') }}" # will return the password value of the item.
+
+```
+<details>
+<summary>View output registered to the `op_item` variable</summary>
+<br>
+
+```
+op: [localhost] => {
+    msg: "var is thepassword"
+}
+```
+</details>
+
+
+**Update an Item**
+
+**❗️Note❗** There is currently no functionality to update vault items. If you wish, use the offical [onepassword.connect](https://github.com/1Password/ansible-onepasswordconnect-collection) collection for this.
+
+## About 1Password
+
+[**1Password**](https://1password.com) is a privacy-focused password manager that keeps you safe online.
+
---
+++ plugins/lookup/opconnect.py
@@ -0,0 +1,239 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2022, Fredrik Lundhag <f@mekk.com>
+# GNU General Public License v3.0+ (see https://www.gnu.org/licenses/gpl-3.0.txt)
+
+DOCUMENTATION = """
+    lookup: opconnect
+    author: Fredrik Lundhag (@joltcan)
+    version_added: "2.11"
+    short_description: retreive information from vault(s) in 1Password via 1Password connect
+    description: |
+        Retreive information from vaults in 1Password via 1Password Connect (opconnect). An opconnect instance is needed,
+        see https://github.com/1Password/connect
+    options:
+      _terms:
+        description: Vault item too look for
+        required: True
+      vault:
+        description: Name of the vault that the item resides in.
+        required: True
+      section:
+        description: Item section, can be used with field below.
+        required: False
+      field:
+        description: field to search for. Needed when section is used
+        required: False
+      op_connect_host:
+        description: REST API endpoint for 1Password Connect
+        env:
+          - name: OP_CONNECT_HOST
+        ini:
+          - section: op_connect
+            key: op_connect_host
+        required: True
+        type: string
+      op_connect_token:
+        description: API token for 1Password Connect access
+        env:
+          - name: OP_CONNECT_TOKEN
+        ini:
+          - section: op_connect
+            key: op_connect_token
+        required: True
+        type: string
+      op_connect_skip_verify:
+        description: Skip TLS host verification
+        env:
+          - name: OP_CONNECT_SKIP_VERIFY
+        ini:
+          - section: op_connect
+            key: op_connect_skip_verify
+        type: boolean
+        required: False
+      op_connect_ca_bundle:
+        description: Custom CA bundle for self signed certificates
+        env:
+          - name: OP_CONNECT_CA_BUNDLE
+        ini:
+          - section: op_connect
+            key: op_connect_ca_bundle
+        required: False
+        type: boolean
+"""
+
+EXAMPLES = """
+---
+- hosts: 127.0.0.1
+  tasks:
+    - set_fact: foo_password="{{ lookup('opconnect', 'item', vault='OPS', section='creds', field='api_key') }}"
+    - debug: msg="var is {{ foo_password }} "
+    - debug: msg="{{ lookup('opconnect', 'item', vault='OPS') }}" # will return the password value of the item.
+
+# Generic lookup
+{{ lookup('opconnect', 'item', vault='OPS', section='creds', field='api_key') }} # foobar
+
+"""
+
+RETURN = """
+  _list:
+    description: 1Password item value
+"""
+
+import os
+import json
+import requests
+
+from ansible.errors import AnsibleError, AnsibleParserError
+from ansible.module_utils.common.text.converters import to_native
+from ansible.plugins.lookup import LookupBase
+from ansible.module_utils._text import to_text
+from ansible.utils.display import Display
+display = Display()
+
+class LookupModule(LookupBase):
+    def run(self, terms, variables=None, **kwargs):
+        # First of all populate options,
+        # this will already take into account env vars and ini config
+        self.set_options(var_options=variables, direct=kwargs)
+        
+        display.vvvv(u"1Password Connect lookup initial terms is %s" % (terms))
+        # populate our options
+        server = self.get_option('op_connect_host')
+        token  = self.get_option('op_connect_token')
+        skipverify = self.get_option('op_connect_skip_verify')
+        cabundle = self.get_option('op_connect_ca_bundle')
+
+        # verify that we have a proper token, and get a list of vaults since we need them
+        # in the next step anyway
+        try:
+            vaults = self._get_vaults(server, token, cabundle, skipverify)
+        except:
+            raise AnsibleError('ERROR: %s' % vaults)
+
+        # find the vault we need
+        for _vault in vaults:
+            if _vault['name'].lower() == self.get_option('vault').lower():
+                vaultuuid = _vault['id']
+        if not vaultuuid:
+            raise AnsibleError('ERROR: Vault uuid not found')
+        
+        # convert or options to vars
+        itemname = terms[0]
+        section  = self.get_option('section')
+        field    = self.get_option('field')
+        if section != None:
+            display.vvvv:("Using section: %s" % section)
+
+        display.vvvv(u"opconnect lookup using item %s, vault uuid: %s" % (itemname, vaultuuid))
+        
+        result = []
+        # get our item/value
+        try:
+            item = self._get_item(server, token, itemname, vaultuuid, section, field, cabundle, skipverify)
+            result.append(item.rstrip())
+        except:
+            raise AnsibleError('ERROR: lookup of the value failed, could not be found. Maybe try and specify section or field?')
+
+        return result
+
+    def _get_item(self, server, token, itemname, vaultuuid, section, field, cabundle, skipverify):
+        # https://support.1password.com/connect-api-reference/#list-items
+        itemuuid = False
+        headers = { 'Authorization': 'Bearer ' + token,
+                   'Content-type': 'application/json'
+                  }
+
+        # TODO: Get payload filter to work
+        # params = {'filter' : 'title eq "' + itemname + '"'}
+        # https://support.1password.com/connect-api-reference/#list-items
+        params = {}
+        url = server + '/v1/vaults/' + vaultuuid + '/items'
+        try:
+            if skipverify != None:
+                response = requests.get(url, headers=headers, params=params, verify=False)
+            elif cabundle != None:
+                response = requests.get(url, headers=headers, params=params, verify=cabundle)
+            else:
+                response = requests.get(url, headers=headers)
+        except Exception as e:
+            raise AnsibleError('ERROR: Can not reach "%s": error: %s' % (server, to_native(e)))
+            
+        data = json.loads(response.content)
+        if not response.ok:
+            raise AnsibleError('Failed to communicate with opconnect: %s' % data['message'])
+
+        # iterate and find the item uuid
+        for _item in data:
+            if _item['title'].lower() == itemname.lower():
+                itemuuid = _item['id']
+        if not itemuuid:
+            raise AnsibleError('Couldn\'t find the uuid for %s in 1Password connect' % itemname)
+
+        display.vvvv('Section: %s, uuid: %s' % (section, itemuuid))
+
+        # now get the entry with the vaultuiid and itemuuid
+        # GET /v1/vaults/{vaultUUID}/items/{itemUUID}
+        url = server + '/v1/vaults/' + vaultuuid + '/items/' + itemuuid
+        try:
+            if skipverify != None:
+                response = requests.get(url, headers=headers, verify=False)
+            elif cabundle != None:
+                response = requests.get(url, headers=headers, verify=cabundle)
+            else:
+                response = requests.get(url, headers=headers)
+        except Exception as e:
+            raise AnsibleError('ERROR: Can not reach "%s": error: %s' % (server, to_native(e)))
+          
+        data = json.loads(response.content)
+        if not response.ok:
+            raise AnsibleError('Failed to communicate with opconnect: %s' % data['message'])
+
+        # iterate and find the item uuid
+        itemvalue = False
+        sectionid = False
+
+        for item in data['fields']:
+            if section != None and "section" in item:
+                if item['section']['label'] == section and item['label'] == field:
+                    itemvalue = item['value']
+            else:
+                # ignore section if we aren't looking for it
+                if "section" in item:
+                    continue
+
+                # try to get a field without section
+                if field != None:
+                    if item['label'] == field:
+                         itemvalue = item['value']
+
+                # if no field or section, just get the password by default
+                # as the onepassword plugin does
+                else:
+                    if item['id'] == "password" and item['label'] == "password":
+                        itemvalue = item['value']
+
+        return(itemvalue)
+
+    def _get_vaults(self, server, token, cabundle, skipverify):
+        # https://support.1password.com/connect-api-reference/#list-vaults
+        # GET /v1/vaults
+        headers = { 'Authorization': 'Bearer ' + token,
+                   'Content-type': 'application/json'
+                  }
+        try:
+            if skipverify != None:
+                response = requests.get(server + '/v1/vaults', headers=headers, verify=False)
+            elif cabundle != None:
+                response = requests.get(server + '/v1/vaults', headers=headers, verify=cabundle)
+            else:
+                response = requests.get(server + '/v1/vaults', headers=headers)
+        except Exception as e:
+            raise AnsibleError('ERROR: Can not reach "%s": error: %s' % (server, to_native(e)))
+
+        vaults = json.loads(response.content)
+        if not response.ok:
+            raise AnsibleError('ERROR: %s' % vaults['message'])
+
+        return vaults
